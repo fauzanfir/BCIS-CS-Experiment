631579fc68cde9e921466d35aa9565ffcd591693
fauzan.firzandy <fauzan.firzandy@ui.ac.id>
2023-11-05 19:08:27 +0700

    Dataset generator

---
+++ generate_dataset.py
@@ -0,0 +1,13 @@
+import random
+def generate(num):
+    file = open(str("dataset_" + str(num) + ".txt"), "w+")
+    i = 0
+    while i < num:
+        val = random.randint(0, 1000000)
+        file.writelines(str(val) + "\n")
+        i += 1
+
+# generate(500)
+# generate(5000)
+# generate(50000)
+generate(3000)
